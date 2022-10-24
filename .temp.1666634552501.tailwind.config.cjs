3d199e1f221caa62c382c2a97f00c102008b5970
BuildTools <unconfigured@null.spigotmc.org>
2022-10-24 19:27:54 +0100

    Sorted out the buttons to be better colors (teal and cyan). Also added a hero component that is customizable by giving it props, and added an image although image needs work.

--- .temp.1666634552501.tailwind.config.cjs
+++ .temp.1666634552501.tailwind.config.cjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import "@fontsource/m-plus-rounded-1c";
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		extend: {
+			fontFamily: {
+
+			},
+		},
 	},
 	plugins: [],
 }
