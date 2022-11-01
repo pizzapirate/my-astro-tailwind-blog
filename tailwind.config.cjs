ae5bf45c7add9377e2009189af63641d881a1485
BuildTools <unconfigured@null.spigotmc.org>
2022-11-01 14:39:05 +0000

    Finished the first draft of my website!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		extend: {
+			animation: {
+				'bounce-slow': 'bounce 2s linear infinite',
+			}
+		}
 	},
 	plugins: [],
 }
