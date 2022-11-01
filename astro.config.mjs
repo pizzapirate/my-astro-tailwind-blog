887760206a7ecaef1de8c37f8921899a410bfbbe
BuildTools <unconfigured@null.spigotmc.org>
2022-11-01 14:40:59 +0000

    Adding Vercel intigration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
